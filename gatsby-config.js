6e85a9dd4f649907f25282ffb917fe2c919cac1b
Edward Whelan <vroxing@gmail.com>
2018-12-28 14:13:19 -0500

    edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Disco Paw`,
+    description: `A site for Disco Paw and its listeners`,
+    author: `@edwhelan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
