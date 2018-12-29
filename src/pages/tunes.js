6c06044fac4af069fc8ee2dde10e7de18b6a301a
Edward Whelan <vroxing@gmail.com>
2018-12-29 17:40:47 -0500

    adds image for tunes page

--- src/pages/tunes.js
+++ src/pages/tunes.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import handshake from '../images/handshake.jpg'
 
 const TunesPage = () => (
   <Layout>
@@ -9,6 +10,9 @@ const TunesPage = () => (
     <div className='tunes-page'>
       <p>More tunes and visuals coming soon, if your thirst is too strong send Disco Paw an email and a present will be teleported to your inbox</p>
       <a href="mailto:discopaw@gmail.com">discopaw@gmail.com</a>
+    </div>
+    <div className='story-page'>
+      <img src={handshake} alt='disco paw and justin shaking hands' />
     </div>
   </Layout>
 )
