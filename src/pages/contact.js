55471ae9dd50d8732a373ba2faa0321e278beafe
Edward Whelan <vroxing@gmail.com>
2018-12-29 12:47:31 -0500

    fix issue with justin picture

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import JustinImage from '../images/Justin.jpeg'
+import JustinImage from '../images/justin.jpeg'
 import DiscoImage from '../images/discopaw.jpeg'
 
 const ContactPage = () => (
