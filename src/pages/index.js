ef0836afa68f3924a2cf73256ccea20b631d4c29
Edward Whelan <vroxing@gmail.com>
2018-12-29 17:48:56 -0500

    add contact form in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import ContactForm from '../components/ContactForm';
 
 
 const IndexPage = () => (
@@ -13,7 +12,17 @@ const IndexPage = () => (
       <iframe width="560" height="315" src="https://www.youtube.com/embed/IVDGQ7JoEc8?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
     {/* <Link to="/page-2/">Go to page 2</Link> */}
-    <ContactForm />
+    <footer>
+      <form className='footer' name='contact' method="post" data-netlify='true' data-netlify-honeypot='bot-field'>
+        <input type='hidden' name='bot-field' />
+        <div className="contact-text">
+          <label htmlFor="email">Your Email = Free Tunes + More Dance</label>
+          <br />
+          <input className='text-area' type="email" name="email" id="email" />
+        </div>
+        <input className='form-butt' type="submit" value="Get Tunes" />
+      </form>
+    </footer>
   </Layout>
 )
 
